008a44e9d60d1652ab8e2f3dfe4f5f9018edb695
Samuel095383 <105554130+Samuel095383@users.noreply.github.com>
2026-03-28 10:09:26 +0100

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_speech/FlutterSpeechRecognitionPlugin.h>)
+#import <flutter_speech/FlutterSpeechRecognitionPlugin.h>
+#else
+@import flutter_speech;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterSpeechRecognitionPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterSpeechRecognitionPlugin"]];
+}
+
+@end
